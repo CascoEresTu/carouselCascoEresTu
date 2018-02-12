a5aca5f7ec9912a7c87328f8fbc09b3302475060
CascoEresTu <cgcascovalle@unitec.edu>
2018-02-12 16:58:54 -0600

    cosas

---
+++ scripts.js
@@ -0,0 +1,40 @@
+function isClicked(n){
+    document.getElementById("inDisplay").src = document.getElementById('pic'+n).src;
+    if (n ==1 ) {
+
+    }
+
+
+    if (n ==2 ) {
+
+    }
+
+    if (n ==3 ) {
+
+    }
+
+    if (n ==4 ) {
+
+    }
+
+    if (n ==5 ) {
+
+
+    }
+
+}
+
+
+function LeftClicked(){
+  var s = document.getElementsById('propic');
+  s.remove()
+
+}
+
+
+
+
+function RightClicked(){
+
+
+}
